6ed53912e47b51fba4f3317dd8f25b016c81138d
Imran Hossen <imranhossen20@gmail.com>
2022-06-27 21:41:48 +0600

    final my project

--- src/pages/Dashboard/UserProfile.js
+++ src/pages/Dashboard/UserProfile.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import auth from '../../firebase.init';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
-import { Navigate } from 'react-router';
+import { useAuthState, useUpdateProfile } from 'react-firebase-hooks/auth';
 
 const UserProfile = () => {
     const [user] = useAuthState(auth)
+    const [updateProfile] = useUpdateProfile(auth);
+
+    const handleUpdate = async (event) => {
+        event.preventDefault()
+        await updateProfile({ displayName: event.target.name.value });
+        alert('Updated profile');
+    }
 
 
     return (
         <>
             <h1 className='text-xl font-bold text-primary ml-10 mt-5'> User Profile</h1>
+            <form onSubmit={handleUpdate}>
+                <div className='form-control'>
+                    <div className="input-group mb-5 mt-5">
+                        <span>Name</span>
+                        <input name='name' type="text" defaultValue={user.displayName} class="input input-bordered" />
+                    </div>
+                    <div className="input-group mb-5">
+                        <span>Email</span>
+                        <input type="text" defaultValue={user.email} class="input input-bordered" />
 
-            <div className='form-control'>
-                <div className="input-group mb-5 mt-5">
-                    <span>Name</span>
-                    <input type="text" value={user.displayName} class="input input-bordered" />
+                    </div>
+                    <button type='submit' className="btn btn-secondary w-48 ml-5">Update</button>
                 </div>
-                <div className="input-group mb-5">
-                    <span>Email</span>
-                    <input type="text" value={user.email} class="input input-bordered" />
-                </div>
-                <button className="btn btn-secondary w-48 ml-5">Update</button>
-            </div>
+            </form>
         </>
 
 
